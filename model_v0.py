a32c4a33178fbb1e88996cf006120af338d0aa8a
mgteus <mateus.guimaraes048@gmail.com>
2025-02-14 08:08:30 -0300

    ajustando primeiro commit.

---
+++ model_v0.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import torch 
+
+
+
+
+
+# montar o dataloader conforme o projeto do mestrado
+# buscar uma transformer vanilla no huggingface e etc
+# treinar modelo
+# analisar métricas
+# testar geracao de textos (conforme requisitado)
+def main():
+
+    
+    return
+
+# estudar como gerar os predict no formato {x0, xf} -> modelo([x0, xf]) -> {x0, x1, x2, ..., xf}
+
+
+if __name__ == '__main__':
+    main()
